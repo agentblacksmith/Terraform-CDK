46725a64c182bd61b0203b21b4f616a5f50e26ca
agentblacksmith <agentblaksmith@gmail.com>
2022-12-30 03:06:03 +0530

    added policy resources and did some cleanup

--- README.md
+++ README.md
@@ -1,4 +1,32 @@
 # Terraform CDK project with Opensearch, DynamoDB
-### Table of Content
+## Table of Content
+* [Project Assumption](#project-assumptions)
+* [About DynamoDB](#dyanmodb)
 
-This project motive is to create
+This project motive is to create a stack with AWS opensearch, Dynamodb. 
+The Stack includes, 
+1. DynamoDB Table
+2. Lambda Function
+3. Opensearch Domain
+---
+## Project Assumptions
+1. The roles and policies were created prior to the project. The ARN's for the resources were assumeed with the resource name combined in the [AWS ARN format](https://docs.aws.amazon.com/general/latest/gr/aws-arns-and-namespaces.html)
+2. The VPC, subnet and security groups were created before the project and utilized here. These can also be automated and in the future versions of this project we can see that. Also only one subnet was created, this was accidently done. Will be creating more.
+3. The opensearch is only single zone to reduce the additional costs.
+4. Most of the variables are defined in the same file which can also be changed to a configuration file and loaded at run time.
+
+---
+## DyanmoDB
+In here I made use of one Dynamdb table with with a Partition key and a sorting key. There were no global secondary index created but it is a good practice to have them on the production usages.
+
+
+
+
+
+
+
+
+The current project is used a single stack and it is not a best practice to write everything in a single Class init method. This area need improvement and need to check what are the possibilities to make the methods more general.
+
+This stack didn't utilize the terraform modules yet, these are something that I have read about and I strongly believe we have to use it. From what I have read so far these modules can be included as part of the `cdktf.json` file. Need more insight in these.
+
--- main.py
+++ main.py
@@ -11,14 +11,27 @@ from cdktf_cdktf_provider_aws.cloudwatch_log_group import CloudwatchLogGroup
 from cdktf_cdktf_provider_aws.opensearch_domain import OpensearchDomain, OpensearchDomainDomainEndpointOptions, OpensearchDomainEbsOptions
 from cdktf_cdktf_provider_aws.opensearch_domain_policy import OpensearchDomainPolicy
 from cdktf_cdktf_provider_aws.iam_role import IamRole
+from cdktf_cdktf_provider_aws.iam_policy import IamPolicy
+from cdktf_cdktf_provider_aws.iam_role_policy_attachment import IamRolePolicyAttachment
+
 
 # AWS variables
 AWS_REGION = os.environ.get("AWS_REGION", "us-east-1")
 Account_ID = os.environ.get("ACCOUNT_ID", "865227664036")
 
 # IAM variables
-Service_role = os.environ.get("SERVICE_ROLE","dynamodb-opeansearch-stream-lambda-role-ocu941je")
-Service_role_arn = f"arn:aws:iam::{Account_ID}:role/service-role/{Service_role}"
+# Service_role = os.environ.get("SERVICE_ROLE","dynamodb-opeansearch-stream-lambda-role-ocu941je")
+# Service_role_arn = f"arn:aws:iam::{Account_ID}:role/service-role/{Service_role}"
+with open(os.path.abspath('policy.json')) as policy_doc:
+    Policy_doc = json.load(policy_doc)
+with open(os.path.abspath('lambda_assume_policy.json')) as policy_doc:
+    Assume_policy = json.load(policy_doc)
+with open(os.path.abspath('opensearch_polciy.json')) as policy_doc:
+    Opensearch_policy = json.load(policy_doc)
+
+IAM_resource_ID = "lambda-opensearch-dynamdb"
+IAM_policy_name = f"{IAM_resource_ID}-iam"
+IAM_role_name = f"{IAM_resource_ID}-role"
 
 # Vpc variables
 Vpc_configs_lambda = {
@@ -29,7 +42,6 @@ Vpc_configs_opensearch = {
     "subnet_ids": ["subnet-01623eca4025f6072"],
     "security_group_ids": ["sg-0d62ff1ebe26cd0d5"]
 }
-
 
 # DynamoDB varialbes
 DynamoDB_Table = "GameScores"
@@ -43,6 +55,8 @@ Lambda_Function_Name = "dynamodbStreamFunction"
 Lambda_Function_Log_Group = f"/aws/lambda/{Lambda_Function_Name}"
 Lambda_Function_Handler = "sample.handler"
 Lambda_Function_Payload = "lambda-opensearch.zip"
+Lambda_Function_Timeout = 30
+Lambda_log_retention = 3
 
 # Opensearch variables
 Opensearch_domain = "gamescores-domain"
@@ -55,30 +69,35 @@ Opensearch_zone_awareness_enabled = False
 Opensearch_ebs_enabled = True
 Opensearch_volume_size = 10
 Opensearch_ttl_policy = "Policy-Min-TLS-1-2-2019-07"
-Opensearch_domain_arn = f"arn:aws:es:us-east-1:{Account_ID}:domain/"
-Opensearch_policy = {
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "AWS": Service_role_arn
-      },
-      "Action": "es:DescribeDomain,es:ESHttp*",
-      "Resource": f"{Opensearch_domain_arn}{Opensearch_domain}/*"
-    }
-  ]
-}
+# Opensearch_domain_arn = f"arn:aws:es:us-east-1:{Account_ID}:domain/"
+
 
 class MyStack(TerraformStack):
     def __init__(self, scope: Construct, ns: str):
         super().__init__(scope, ns)
         AwsProvider(self, "AWS", region=AWS_REGION)
 
-        
+        # IAM Policy
+        iam_policy = IamPolicy(self,
+                               IAM_policy_name,
+                               name=IAM_policy_name,
+                               description='lambda to handle dynamodb stream and opensearch',
+                               policy=json.dumps(Policy_doc)
+                               )
+        iam_role = IamRole(self,
+                           IAM_role_name,
+                           name=IAM_role_name,
+                           description='Role to give permission for lambda to access different resources',
+                           assume_role_policy=json.dumps(Assume_policy)
+                           )
+        IamRolePolicyAttachment(self, "Role-policy-attachment",
+                                role=iam_role.name,
+                                policy_arn=iam_policy.arn
+                                )
+
         # Cloudwatch log group creation
-        cloudwatchLogGroup = CloudwatchLogGroup(
-            self, "LogGroup", name=Lambda_Function_Log_Group, retention_in_days=3)
+        CloudwatchLogGroup(self, "LogGroup", name=Lambda_Function_Log_Group,
+                           retention_in_days=Lambda_log_retention)
 
         # DynamoDB Table
         dynamodb_table = DynamodbTable(self, "dynamodb",
@@ -88,7 +107,7 @@ class MyStack(TerraformStack):
                                        range_key=DynamoDB_Sortkey,
                                        attribute=[
                                            {
-                                             "name": DynamoDB_Partion_Key,
+                                               "name": DynamoDB_Partion_Key,
                                                "type": DynamoDB_Attribute_Type
                                            },
                                            {
@@ -101,7 +120,8 @@ class MyStack(TerraformStack):
                                        )
 
         # OpenSearch Domain creation
-        opensearch_domain = OpensearchDomain(self, "opensearch_domain", domain_name=Opensearch_domain,
+        opensearch_domain = OpensearchDomain(self, Opensearch_domain,
+                                             domain_name=Opensearch_domain,
                                              engine_version=Opensearch_version,
                                              vpc_options=Vpc_configs_opensearch,
                                              cluster_config={
@@ -111,48 +131,52 @@ class MyStack(TerraformStack):
                                                  "instance_type": Opensearch_instance_type,
                                                  "zone_awareness_enabled": Opensearch_zone_awareness_enabled
                                              },
-                                             access_policies=json.dumps(Opensearch_policy),
+                                             #  access_policies         = json.dumps(Opensearch_policy),
                                              domain_endpoint_options=OpensearchDomainDomainEndpointOptions(
                                                  enforce_https=True,
                                                  tls_security_policy=Opensearch_ttl_policy),
                                              ebs_options=OpensearchDomainEbsOptions(
-                                                 ebs_enabled=Opensearch_ebs_enabled, volume_size=Opensearch_volume_size)
-                                            
+                                                 ebs_enabled=Opensearch_ebs_enabled, volume_size=Opensearch_volume_size),
+                                             encrypt_at_rest={
+                                                 "enabled": True
+                                             }
                                              )
-
+        Opensearch_policy["Statement"][0]["Resource"] = opensearch_domain.arn
+        opensearch_domain.access_policies = json.dumps(Opensearch_policy)
 
         # Lambda function creation
-        lambda_function=LambdaFunction(self,
-                                         Lambda_Function_Name,
+        lambda_function = LambdaFunction(self, Lambda_Function_Name,
                                          function_name=Lambda_Function_Name,
                                          runtime="python3.8",
                                          handler=Lambda_Function_Handler,
                                          filename=os.path.abspath(
                                              Lambda_Function_Payload),
-                                         role=Service_role_arn,
-                                         environment=LambdaFunctionEnvironment(variables={
-                                             "ENDPOINT": f"https://{opensearch_domain.endpoint}"
-                                         }),
+                                         role=iam_role.arn,
+                                         environment=LambdaFunctionEnvironment(
+                                             variables={
+                                                 "ENDPOINT": f"https://{opensearch_domain.endpoint}"
+                                             }
+                                         ),
                                          publish=True,
+                                         timeout=Lambda_Function_Timeout,
                                          depends_on=[opensearch_domain],
                                          vpc_config=Vpc_configs_lambda,
-                                         
                                          )
 
         # DynamoDB Stream event source mapping
-        dynamdb_stream_lambda=LambdaEventSourceMapping(self, "lambd_function_stream_trigger",
-                                                               function_name=lambda_function.function_name,
-                                                               event_source_arn=dynamodb_table.stream_arn,
-                                                               starting_position="LATEST"
+        dynamdb_stream_lambda = LambdaEventSourceMapping(self, "lambd_function_stream_trigger",
+                                                         function_name=lambda_function.function_name,
+                                                         event_source_arn=dynamodb_table.stream_arn,
+                                                         starting_position="LATEST"
                                                          )
 
 
-app=App()
-stack=MyStack(app, "learn-cdktf-dynamdb")
-# RemoteBackend(stack,
-#   hostname='app.terraform.io',
-#   organization='example-org-8df812',
-#   workspaces=NamedRemoteWorkspace('learn-cdktf-dynamdb')
-# )
+app = App()
+stack = MyStack(app, "learn-cdktf-dynamdb")
+RemoteBackend(stack,
+              hostname='app.terraform.io',
+              organization='example-org-8df812',
+              workspaces=NamedRemoteWorkspace('learn-cdktf-dynamdb')
+              )
 
 app.synth()
